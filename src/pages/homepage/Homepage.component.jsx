3fa1618f8f71cae11051ba096737a311030f0da0
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-03-21 20:22:34 +0530

    homepage

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const Homepage = () => {
+	return (
+		<div className="homepage">
+			<Directory />
+		</div>
+	);
+};
+
+export default Homepage;
